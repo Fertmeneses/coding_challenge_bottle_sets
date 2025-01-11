b10a310e47c1e96468be093e76755ea02da07346
Fertmeneses_HPvictus <fertmeneses@gmail.com>
2025-01-11 10:04:38 -0300

    Add initial files

---
+++ src/CC_bottle_sets/challenge.py
@@ -0,0 +1,17 @@
+# To be updated
+
+def functionX(s: list) -> bool:
+
+	# Implement your code
+
+	result = None
+
+	return result
+
+# Evaluate challenge (DON'T MODIFY):
+if __name__ == "__main__":
+	example = [1,2,3]
+	if functionX(example):
+		print('valid')
+	else:
+		print('invalid')
